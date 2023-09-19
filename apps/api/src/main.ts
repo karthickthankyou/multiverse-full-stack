e496637fe9bc3b9d13cd405e30df7c659f321b2f
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-09-19 13:32:15 +0530

    add allowed origins in cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { NestExpressApplication } from '@nestjs/platform-express'
-import { AllExceptionsFilter } from './common/filters/allException'
-// Use `PORT` provided in environment or default to 3000
+
 const port = process.env.PORT || 3000
+
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
-  app.enableCors()
-  app.useGlobalFilters(new AllExceptionsFilter())
+  const app = await NestFactory.create(AppModule)
+
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',')
+
+  console.log(allowedOrigins)
+
+  app.enableCors({
+    origin: allowedOrigins,
+    allowedHeaders: '*',
+    methods: '*',
+  })
+
   await app.listen(port, '0.0.0.0')
 }
 bootstrap()
